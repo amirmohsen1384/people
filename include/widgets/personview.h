951fb2867db14422915a782c65d986dc3713846e
amirmohsen1384 <amirmohsen84ghasemi@gmail.com>
2024-10-15 19:30:57 +0330

    Defined the info view

--- include/widgets/personview.h
+++ include/widgets/personview.h
@@ -1,5 +1,7 @@
 #ifndef PERSONVIEW_H
 #define PERSONVIEW_H
+
+#include "include/data/person.h"
 #include <QWidget>
 
 namespace Ui {
@@ -9,11 +11,24 @@ class PersonView;
 class PersonView : public QWidget
 {
     Q_OBJECT
+    Person information;
     Ui::PersonView *ui = nullptr;
+
+private slots:
+    void UpdateName();
+    void UpdatePhoto();
+    void UpdateGender();
+    void UpdateBirthday();
+    void UpdateCreation();
+    void UpdateLastModification();
 
 public:
     explicit PersonView(QWidget *parent = nullptr);
+    explicit PersonView(Person &value, QWidget *parent = nullptr);
     ~PersonView();
+
+    Person& GetPerson();
+    void SetPerson(Person &data);
 };
 
 #endif // PERSONVIEW_H
--- src/widgets/personview.cpp
+++ src/widgets/personview.cpp
@@ -1,9 +1,58 @@
 #include "include/widgets/personview.h"
 #include "ui_personview.h"
 
+void PersonView::UpdateName() {
+    ui->fullName->setText(information.GetFullName());
+}
+
+void PersonView::UpdatePhoto() {
+    ui->photoView->setImage(information.GetPhoto());
+}
+
+void PersonView::UpdateGender() {
+    if(information.GetGender() == Person::Gender::Male) {
+        ui->genderText->setText("Male");
+        ui->genderIcon->setPixmap(":/icons/gender/man.ico");
+    } else {
+        ui->genderText->setText("Female");
+        ui->genderIcon->setPixmap(":/icons/gender/woman.ico");
+    }
+}
+
+void PersonView::UpdateBirthday() {
+    ui->birthdayDate->setText(information.GetBirthday().toString());
+}
+
+void PersonView::UpdateCreation(){
+    ui->creationDate->setText(information.GetCreation().toString());
+}
+
+void PersonView::UpdateLastModification() {
+    ui->lastModifiedDate->setText(information.GetLastModification().toString());
+}
+
 PersonView::PersonView(QWidget *parent) : QWidget(parent), ui(new Ui::PersonView) {
     ui->setupUi(this);
+    connect(&information, &Person::PhotoChanged, this, &PersonView::UpdatePhoto);
+    connect(&information, &Person::GenderChanged, this, &PersonView::UpdateGender);
+    connect(&information, &Person::LastNameChanged, this, &PersonView::UpdateName);
+    connect(&information, &Person::FirstNameChanged, this, &PersonView::UpdateName);
+    connect(&information, &Person::BirthdayChanged, this, &PersonView::UpdateBirthday);
+    connect(&information, &Person::CreationChanged, this, &PersonView::UpdateCreation);
+    connect(&information, &Person::LastModificationChanged, this, &PersonView::UpdateLastModification);
+}
+
+PersonView::PersonView(Person &value, QWidget *parent) : PersonView(parent) {
+    this->SetPerson(value);
 }
 PersonView::~PersonView() {
     delete ui;
+}
+
+Person &PersonView::GetPerson() {
+    return information;
+}
+
+void PersonView::SetPerson(Person &data) {
+    information = data;
 }

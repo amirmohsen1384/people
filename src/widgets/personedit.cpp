7957c843ae05967223149835441bac4efc97ec7b
amirmohsen1384 <amirmohsen84ghasemi@gmail.com>
2024-10-18 10:21:05 +0330

    Fixed a typo

--- src/widgets/personedit.cpp
+++ src/widgets/personedit.cpp
@@ -10,7 +10,7 @@ PersonEdit::PersonEdit(QWidget *parent) : QWidget(parent), ui(new Ui::PersonEdit
     ui->firstNameEdit->setValidator(new FirstNameValidator());
     ui->lastNameEdit->setValidator(new LastNameValidator());
 
-    connect(&photographer, &Photographer::AvaliableDevicesChanged, this, &PersonEdit::UpdatePhotographerControl);
+    connect(&photographer, &Photographer::AvailableDevicesChanged, this, &PersonEdit::UpdatePhotographerControl);
 
     connect(ui->firstNameResetButton, &QPushButton::clicked, this, &PersonEdit::ResetFirstName);
     connect(ui->lastNameResetButton, &QPushButton::clicked, this, &PersonEdit::ResetLastName);

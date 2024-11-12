b502d122b6af831b44fecee102b4de75f8d31b6b
amirmohsen1384 <amirmohsen84ghasemi@gmail.com>
2024-11-12 19:48:20 +0330

    Tested the program

--- main.cpp
+++ main.cpp
@@ -2,9 +2,14 @@
 #include "include/application.h"
 #include <QApplication>
 #include <QFile>
+#include "include/dialogs/personeditdialog.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    PersonEditDialog dialog;
+    dialog.exec();
+
     return app.exec();
 }

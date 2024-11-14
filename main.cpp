72dd6b79ce8ba93c5ecf4e55168d784b53f3bb95
amirmohsen1384 <amirmohsen84ghasemi@gmail.com>
2024-11-14 10:03:48 +0330

    Fixed a compile error in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,12 @@
-#include "include/data/person.h"
 #include "include/application.h"
 #include <QApplication>
-#include <QFile>
-#include "include/dialogs/personeditdialog.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    PersonEditDialog dialog;
-    dialog.exec();
+    Application window;
+    window.show();
 
     return app.exec();
 }

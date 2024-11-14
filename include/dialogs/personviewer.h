e24a69981d6b0cd78bf7cd5fbb7722c8ae861ad2
amirmohsen1384 <amirmohsen84ghasemi@gmail.com>
2024-11-14 14:05:36 +0330

    Implemented the viewer

--- include/dialogs/personviewer.h
+++ include/dialogs/personviewer.h
@@ -2,6 +2,7 @@
 #define PERSONVIEWER_H
 
 #include <QDialog>
+#include "include/data/person.h"
 
 namespace Ui {
 class PersonViewer;
@@ -10,10 +11,30 @@ class PersonViewer;
 class PersonViewer : public QDialog
 {
     Q_OBJECT
+    const QSize size = QSize(900, 900);
     Ui::PersonViewer *ui = nullptr;
+
+protected:
+    virtual QSize sizeHint() const;
+
+protected slots:
+    void UpdateTitle();
+    void RequestToEdit();
+
 public:
+    explicit PersonViewer(const Person &person, QWidget *parent = nullptr);
     explicit PersonViewer(QWidget *parent = nullptr);
     ~PersonViewer();
+
+    Person GetPerson() const;
+
+public slots:
+    void SetPerson(const Person &person);
+
+signals:
+    void PersonChanged(const Person &data);
+    void EditRequested(const Person &data);
+
 };
 
 #endif // PERSONVIEWER_H
--- src/dialogs/personviewer.cpp
+++ src/dialogs/personviewer.cpp
@@ -1,9 +1,48 @@
 #include "include/dialogs/personviewer.h"
 #include "ui_personviewer.h"
 
+PersonViewer::PersonViewer(const Person &person, QWidget *parent) : PersonViewer(parent) {
+    this->SetPerson(person);
+}
+
 PersonViewer::PersonViewer(QWidget *parent) : QDialog(parent), ui(new Ui::PersonViewer) {
     ui->setupUi(this);
+
+    this->setFixedSize(size);
+    connect(ui->editButton, &QPushButton::clicked, this, &PersonViewer::RequestToEdit);
+
+    connect(this, &PersonViewer::PersonChanged, this, &PersonViewer::UpdateTitle);
+    UpdateTitle();
 }
+
 PersonViewer::~PersonViewer() {
     delete ui;
+}
+
+QSize PersonViewer::sizeHint() const {
+    return size;
+}
+
+void PersonViewer::UpdateTitle() {
+    const Person &person = ui->viewer->GetPerson();
+    if(person.GetFirstName().isEmpty() || person.GetLastName().isEmpty()) {
+        this->setWindowTitle("Unnamed Person");
+
+    } else {
+        this->setWindowTitle(person.GetFullName());
+
+    }
+}
+
+void PersonViewer::RequestToEdit() {
+    emit EditRequested(ui->viewer->GetPerson());
+}
+
+Person PersonViewer::GetPerson() const {
+    return ui->viewer->GetPerson();
+}
+
+void PersonViewer::SetPerson(const Person &person) {
+    ui->viewer->SetPerson(person);
+    emit PersonChanged(person);
 }

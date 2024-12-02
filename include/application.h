3246e92cb08dcf86d2b9de6880a621b75a4099f6
amirmohsen1384 <amirmohsen84ghasemi@gmail.com>
2024-12-02 22:04:18 +0330

    Defined some functionality of the project

--- include/application.h
+++ include/application.h
@@ -2,6 +2,7 @@
 #define APPLICATION_H
 
 #include <QMainWindow>
+#include "include/model/peoplemodel.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -12,12 +13,22 @@ QT_END_NAMESPACE
 class Application : public QMainWindow
 {
     Q_OBJECT
+    PeopleModel model;
+    Ui::Application *ui;
+
+protected slots:
+    bool Load(const QString &filename);
+    bool Save(const QString &filename);
+
+    void Add();
+    void Save();
+    void Clear();
+    void SaveAs();
+    void Edit(const QModelIndex &index);
+    void Delete(const QModelIndexList &indices);
 
 public:
     Application(QWidget *parent = nullptr);
     ~Application();
-
-private:
-    Ui::Application *ui;
 };
 #endif // APPLICATION_H

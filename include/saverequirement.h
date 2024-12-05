0e9c58590373336ceeb22b0d4876dd659e5e0a8e
amirmohsen1384 <amirmohsen84ghasemi@gmail.com>
2024-12-05 10:27:06 +0330

    Defined the save mechanism

--- include/saverequirement.h
+++ include/saverequirement.h
@@ -2,12 +2,23 @@
 #define SAVEREQUIREMENT_H
 
 #include <QObject>
+#include <QMessageBox>
 
 class SaveRequirement : public QObject
 {
     Q_OBJECT
+    bool required;
+
 public:
-    explicit SaveRequirement(QObject *parent = nullptr);
+    explicit SaveRequirement(bool initial = false, QObject *parent = nullptr);
+    bool IsRequired() const;
+
+public slots:
+    void SetRequired(bool state);
+    QMessageBox::ButtonRole Notify(QWidget *parent);
+
+signals:
+    void StateChanged(bool requirement);
 };
 
 #endif // SAVEREQUIREMENT_H
--- src/saverequirement.cpp
+++ src/saverequirement.cpp
@@ -1,3 +1,25 @@
 #include "include/saverequirement.h"
 
-SaveRequirement::SaveRequirement(QObject *parent) : QObject{parent} {}
+SaveRequirement::SaveRequirement(bool initial, QObject *parent) :
+    QObject{parent},
+    required{initial}
+{}
+
+bool SaveRequirement::IsRequired() const {
+    return required;
+}
+
+void SaveRequirement::SetRequired(bool state) {
+    required = state;
+    emit stateChanged(state);
+}
+
+QMessageBox::ButtonRole SaveRequirement::Notify(QWidget *parent) {
+    QMessageBox dialog(parent);
+    dialog.setWindowTitle("Save Changes");
+    dialog.setIcon(QMessageBox::Information);
+    dialog.setInformativeText("Do you want to save changes to a file?");
+    dialog.setStandardButtons(QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
+    dialog.exec();
+    return dialog.result();
+}
